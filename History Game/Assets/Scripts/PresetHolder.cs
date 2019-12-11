e82d27e111fc15f5bff6eaa2de2849ce2018c1c0
Milly <milabits@gmail.com>
2019-12-11 16:55:03 +0100

    Downgraded Unity to build the game

--- History Game/Assets/Scripts/PresetHolder.cs
+++ History Game/Assets/Scripts/PresetHolder.cs
@@ -1,6 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using SmartwallPackage.GlobalGameSettings;
 using UnityEngine;
 
 public class PresetHolder : MonoBehaviour
 {
 	public GamePreset gamePreset;
+
+	[SerializeField]
+	private List<ThemeString> themeStrings;
+
+	private void Awake()
+	{
+		GetChosenPreset();
+	}
+
+	private void GetChosenPreset()
+	{
+		string theme = GlobalGameSettings.GetSetting("Theme");
+
+		GamePreset chosenPreset = themeStrings.First(x => x.name.Equals(theme)).preset;
+
+		if (chosenPreset != null) gamePreset = chosenPreset;
+	}
+}
+
+[Serializable]
+public struct ThemeString
+{
+	public string name;
+	public GamePreset preset;
 }
